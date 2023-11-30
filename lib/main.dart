84ef3547b4b9bb431f652cd55397496bb3da7ac0
Tuyen Nguyen <nguyenvantuyen98nd@gmail.com>
2023-11-30 23:52:09 +0700

    Add firebase config for Android

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'dart:async';
+
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+
 import 'app.dart';
 import 'config/injection.dart';
 import 'localization/localization.dart';
 
 FutureOr<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   await setupInjection();
   Localization.initApp(const App());
 }
